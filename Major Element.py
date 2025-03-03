c382fb136e3f1ed8aaa3b7b1ac62982de6958f65
Yodit Debebe <amenyene777@gmail.com>
2025-03-03 09:13:12 +0300

    Update Major Element.py

--- Major Element.py
+++ Major Element.py
@@ -2,7 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         counts = {}
         n = len(nums)
-        for i in nums:
-            counts[i] = counts.get(i, 0) + 1
-            if counts[i] > n//2:
-                return i
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+            if counts[num] > n//2:
+                return num
